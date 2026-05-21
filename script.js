940cdc27527334764d296435c033634bd18c9891
Usman-ali589 <um139358@gmail.com>
2026-05-21 23:39:20 +0500

    Implement weather search functionality
    Added a function to search and display weather information for a specified city.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Smart Weather App Started");
+function searchWeather() {
+
+    const city = document.getElementById("cityInput").value;
+
+    if(city === "") {
+        alert("Please enter a city name");
+        return;
+    }
+
+    document.getElementById("cityName").innerText = city;
+
+    document.getElementById("temperature").innerText =
+    "Temperature data will appear here";
+
+    document.getElementById("condition").innerText =
+    "Weather condition will appear here";
+
+}
